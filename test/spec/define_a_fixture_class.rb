97802fe0a53062be21f04bbd138d828e7335d937
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2015-05-11 11:09:43 -0500

    Update tests for defining a fixture class.

--- test/spec/define_a_fixture_class.rb
+++ test/spec/define_a_fixture_class.rb
@@ -1,21 +1,25 @@
 require_relative 'spec_init'
 
-define_a_fixture_class = Fixture.define do
+define_a_fixture = Fixture.define do
   def self.example_class
     define_class do
     end
   end
 end
 
-describe 'Define a fixture class' do
+describe 'Defining a fixture class' do
+  specify 'Creates a module' do
+    assert(define_a_fixture.is_a? Module)
+  end
+
   specify 'Creates a class' do
-    example_class = define_a_fixture_class.example_class
+    example_class = define_a_fixture.example_class
 
     assert(example_class.is_a? Class)
   end
 
   specify 'The created class is within the Fixture namespace' do
-    example_class = define_a_fixture_class.example_class
+    example_class = define_a_fixture.example_class
 
     assert(example_class.const_defined? :Fixture)
   end
